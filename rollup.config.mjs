1c8d51d0381d6df7ef12043c06c849054033244a
pearlpandz <pearlpandzz@gmail.com>
2023-08-04 14:42:55 +0530

    rollup done

---
+++ rollup.config.mjs
@@ -0,0 +1,39 @@
+import external from 'rollup-plugin-peer-deps-external';
+import { babel } from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import postcss from 'rollup-plugin-postcss';
+import autoprefixer from 'autoprefixer';
+import image from '@rollup/plugin-image';
+import React from 'react';
+
+export default [
+  {
+    input: 'src/index.js',
+    output: {
+      dir: 'dist',
+      format: 'cjs'
+    },
+    plugins: [
+      external(),
+      babel({
+        babelHelpers: 'bundled',
+        extensions: [".js", ".jsx"],
+        presets: ["@babel/preset-react"],
+      }),
+      commonjs(),
+      resolve({ 
+        extensions: ['.js', '.jsx'],
+        moduleDirectories: ['node_modules']
+      }),
+      postcss({
+        plugins: [autoprefixer()],
+        sourceMap: true,
+        extract: true,
+        minimize: true
+      }),
+      image()
+    ],
+    external: ["react", "react-dom"],
+  }
+];
